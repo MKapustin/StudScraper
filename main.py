e91ec0ebe1bdc2ec8d60d4884d8ec7aeb5b94f4f
MKapustin <maksim.kapustsin@codex-soft.com>
2020-07-10 19:27:04 +0300

    Lockers-with-lists method was changed with thread-safe queue

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 from concurrent.futures import ThreadPoolExecutor
-from threading import Lock
 from xlwt import Workbook
+from queue import Queue
 
 
 def get_page_soup(url):
@@ -14,10 +14,13 @@ def get_page_soup(url):
 def get_all_pagination_links(url):
     main_page_soup = get_page_soup(url)
     if main_page_soup:
+        pagination_links = Queue()
+        pagination_links.put(url)
         pagination_a_items = main_page_soup.find_all("a", {
             "class": "pagination-link"})
-        return [url] + ["https://www.hertie-school.org" + pagination_a_item.attrs["href"]
-                        for pagination_a_item in pagination_a_items]
+        for pagination_a_item in pagination_a_items:
+            pagination_links.put("https://www.hertie-school.org" + pagination_a_item.attrs["href"])
+        return pagination_links
 
 
 def get_person_email(person_private_url):
@@ -34,38 +37,28 @@ def get_person_email(person_private_url):
         return person_email
 
 
-def add_persons_info_from_pagination_links(persons_info, pagination_links,
-                                           pagination_links_list_locker,
-                                           persons_info_list_locker):
-    while True:
-        if not pagination_links:
-            break
-        with pagination_links_list_locker:
-            pagination_link = pagination_links.pop(0)
+def add_persons_info_from_pagination_links(persons_info, pagination_links):
+    while not pagination_links.empty():
+        pagination_link = pagination_links.get()
         page_soup = get_page_soup(pagination_link)
         for person_div in page_soup.find_all("div", {
             "class": "grid-item col-xs-12 col-sm-6 col-md-3 type-link has-circle-image color-scheme--red"}):
             person_info = [person_div.h2.span.text.strip(),
                            person_div.find("div", {"class": "grid-item-text"}).text.strip(),
                            get_person_email("https://www.hertie-school.org" + person_div.a.attrs["href"])]
-            with persons_info_list_locker:
-                persons_info.append(person_info)
-                print(person_info)
+            persons_info.put(person_info)
 
 
 def get_all_persons_info(
         main_url="https://www.hertie-school.org/en/who-we-are/people?no_cache=1&tx_lfpeopledirectory_list%5BselectedAreas%5D=&tx_lfpeopledirectory_list%5BselectedProgrammes%5D=&tx_lfpeopledirectory_list%5BselectedRoles%5D=&tx_lfpeopledirectory_list%5BselectedThemes%5D=&cHash=d65692a89b447c12e06cd55792edcc7d",
-        workers_amount=3
+        workers_amount=6
 ):
     pagination_links = get_all_pagination_links(main_url)
-    persons_info = []  # [(Name, Status, Link to private page), ... ]
-    if pagination_links:
-        pagination_links_list_locker = Lock()
-        persons_info_list_locker = Lock()
+    persons_info = Queue()  # [(Name, Status, Link to private page), ... ]
+    if not pagination_links.empty():
         with ThreadPoolExecutor(max_workers=workers_amount) as executor:
             for index in range(workers_amount):
-                executor.submit(add_persons_info_from_pagination_links, persons_info, pagination_links,
-                                pagination_links_list_locker, persons_info_list_locker)
+                executor.submit(add_persons_info_from_pagination_links, persons_info, pagination_links)
     return persons_info
 
 
@@ -79,4 +72,4 @@ def write_data_to_excel(data_list):
 
 
 if __name__ == "__main__":
-    write_data_to_excel(get_all_persons_info())
+    write_data_to_excel(list(get_all_persons_info().queue))
